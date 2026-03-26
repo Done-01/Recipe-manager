4384c01c17b63820a3c5f0770dbb5e9d731df8d4
Done-01 <robert-a-dunn@hotmail.com>
2026-03-26 02:35:53 +0000

    centering text

--- resources/views/authenticate/register.blade.php
+++ resources/views/authenticate/register.blade.php
@@ -29,8 +29,9 @@
                     <input id="password_confirmation" type="password" name="password_confirmation" required class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-purple-500 focus:border-purple-500">
                 </div>
 
-                <div class="mt-6">
+                <div class="mt-6 text-center">
                     <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">Register</button>
+                    <p>Have an account? <a href="{{ route('login') }}" class="text-purple-600 hover:text-purple-700"> Login</a></p>
                 </div>
             </form>
         </div>
